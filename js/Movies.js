c084e40b325cd8a700a0472c0b7759cd83f677b7
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-22 14:34:44 -0500

    feat: add movie modal added

--- js/Movies.js
+++ js/Movies.js
@@ -7,23 +7,27 @@ function hideLoading() {
 
 $(document).ready(function () {
 
-fetch("https://changeable-sharp-talk.glitch.me/movies")
-    .then(response => response.json())
-    // .catch(error => console.error(error))
-    .then(listOfMovies =>{
-        hideLoading();
-        let moviesHTML = "";
-        // movies += listOfMovies;
-        // console.log(listOfMovies);
-        listOfMovies.forEach(function(element) {
-            console.log(element)
-            moviesHTML += `<div class="card"><div class="card-body">`
-            moviesHTML += `<h5 class="card-title">Title: ${element.title}</h5>`
-            moviesHTML += `<p class="card-text">Rating: ${element.rating}</p>`
-            moviesHTML += `</div></div>`
-        })
-        $("#movies-cards-container").append(moviesHTML);
-    });
+    fetch("https://changeable-sharp-talk.glitch.me/movies")
+        .then(response => response.json())
+            .then(listOfMovies =>{
+                hideLoading();
+                let moviesHTML = "";
+                listOfMovies.forEach(function(element) {
+                    console.log(element)
+                    moviesHTML += `<div class="card"><div class="card-body">`
+                    moviesHTML += `<h5 class="card-title">Title: ${element.title}</h5>`
+                    moviesHTML += `<p class="card-text">Rating: ${element.rating}</p>`
+                    moviesHTML += `</div></div>`
+                })
+                $("#movies-cards-container").append(moviesHTML);
+            })
+        .catch(error => console.error(error));
+
+    $("#add-btn").click((e) => {
+        e.preventDefault();
+        console.log("Button Clicked")
+        $("#add-modal").modal("show")
+    })
 });
 
 //
