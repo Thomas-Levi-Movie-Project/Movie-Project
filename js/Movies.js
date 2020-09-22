db42d0f780e9b7667a5a7e44455c4192febe861e
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-22 14:06:09 -0500

    feat: added cards to movie AJAX data

--- js/Movies.js
+++ js/Movies.js
@@ -17,12 +17,19 @@ fetch("https://changeable-sharp-talk.glitch.me/movies")
         // console.log(listOfMovies);
         listOfMovies.forEach(function(element) {
             console.log(element)
-            moviesHTML += `<p>Title: ${element.title}</p>`
-            moviesHTML += `<p>Rating: ${element.rating}</p>`
+            moviesHTML += `<div class="card"><div class="card-body">`
+            moviesHTML += `<h5 class="card-title">Title: ${element.title}</h5>`
+            moviesHTML += `<p class="card-text">Rating: ${element.rating}</p>`
+            moviesHTML += `</div></div>`
         })
-        $("#movies-html").append(moviesHTML);
+        $("#movies-cards-container").append(moviesHTML);
     });
 });
 
-
-
+//
+// <div class="card">
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>
+// </div>
